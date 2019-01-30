8645a8d2a898bc59790ba191d666c3cbcbabef8e
matt-codeslo <matt@codeslo.com>
2019-01-30 12:59:21 -0800

    added custom reporter for improved feedback

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const testMe = require('./challenges/Week-01/Day-01/testMe');
 
 jasmine.loadConfigFile('spec/support/jasmine.json');
 
+// Register a Custom Reporter
+const Reporter = require('jasmine-console-reporter');
+jasmine.jasmine.getEnv().addReporter(new Reporter());
+
 jasmine.onComplete(function(passed){
     if(passed){
         let code = ""+testMe;
--- spec/support/Week-01/Day-01/testMe.spec.js
+++ spec/support/Week-01/Day-01/testMe.spec.js
@@ -1,8 +1,13 @@
 const testMe = require('../../../../challenges/Week-01/Day-01/testMe');
 
 describe('testMe functionality',()=>{
+
+    it('is a function',()=>{
+        expect(typeof(testMe)).toBe('function');
+    });
+
     it('returns "<word> is the word."',()=>{
-        expect(testMe('bird')).toBe('bird is the word.');
+        expect(testMe('bird')).toBe('byrd is the word.');
     });
 });
 
