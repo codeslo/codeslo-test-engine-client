fd7ae05887b3dcd71cfe92b14824d794b1a621ae
stillmattwest <stillmattwest@gmail.com>
2019-02-02 14:08:03 -0800

    simplified folder structure

--- challenges/Week-01/Day-01/test.js
+++ challenges/Week-01/Day-01/test.js
@@ -1,4 +1,4 @@
 // do not change code in this file
 const root = require('app-root-path');
-const configRunner = require(root + '/config/configurator');
+const configRunner = require(root + 'application/config/configurator');
 configRunner();
