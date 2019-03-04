c195736e7d73b884b9bf6634c0308400d97e2282
matt-codeslo <matt@codeslo.com>
2019-03-04 15:57:25 -0800

    fix to challenge name

--- challenges/01 Introductory Challenges/01 Create A Function/test.js
+++ challenges/01 Introductory Challenges/01 Create A Function/test.js
@@ -1,5 +1,5 @@
 // enter test name here. Be sure it matches a test name in the database. Add tests via the web interface /addtest
-const testName = "Create a Function";
+const testName = "Create A Function";
 // do not change code below this line
 const root = require('app-root-path');
 const configRunner = require(root + '/application/config/configurator');
