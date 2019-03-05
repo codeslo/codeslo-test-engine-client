2bab37d58cd2237132e820386cce4c1e5e590769
matt-codeslo <matt@codeslo.com>
2019-03-05 15:12:32 -0800

    edit findPizza.spec

--- challenges/02 Beginnging Array Challenges/04 Find Pizza/findPizza.spec.js
+++ challenges/02 Beginnging Array Challenges/04 Find Pizza/findPizza.spec.js
@@ -2,11 +2,11 @@ const func = require('./findPizza.challenge');
 
 describe('Search Me requirements',()=>{
     it('returns true if the array passed to it contains "pizza"',()=>{
-        expect(func(['pizza'])).toBe(true);
+        expect(func(['corn','tacos','pizza','apples'])).toBe(true);
     });
 
     it('returns false if the array passed to it does not contain "pizza"',()=>{
-        expect(func(['broccoli'])).toBe(false);
+        expect(func(['broccoli','green beans','oatmeal','french fries'])).toBe(false);
     });
     
 });
