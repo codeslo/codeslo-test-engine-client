a9bd0c48b943da0e6b9f63c10434dfd55f354f45
matt-codeslo <matt@codeslo.com>
2019-03-06 10:49:58 -0800

    removed user_settings/config.js from tracking

--- user_settings/config.js
+++
@@ -1,9 +0,0 @@
-let settings = {
-    firstName:"Your first name here",
-    lastName:"Your last name here",
-    password:"A simple password you don't use anywhere else"
-   
-    
-}
-
-module.exports = settings;
