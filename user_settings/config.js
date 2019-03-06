ec4c6250e4e57cbb839e59000353e1a0fcd4239f
matt-codeslo <matt@codeslo.com>
2019-03-05 18:07:52 -0800

    fixed config.js

--- user_settings/config.js
+++ user_settings/config.js
@@ -1,10 +1,8 @@
 let settings = {
-    // firstName:"Your first name here",
-    // lastName:"Your last name here",
-    // password:"A simple password you don't use anywhere else"
-    firstName:"Lynne",
-    lastName:"West",
-    password:"123"
+    firstName:"Your first name here",
+    lastName:"Your last name here",
+    password:"A simple password you don't use anywhere else"
+   
     
 }
 
